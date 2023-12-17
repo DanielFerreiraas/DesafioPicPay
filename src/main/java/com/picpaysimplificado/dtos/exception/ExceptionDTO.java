7a55800ba7dcf6d895b9d22bc31321043651f6c9
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-12-17 14:16:54 -0300

    feat: Implementação de Exceptions

---
+++ src/main/java/com/picpaysimplificado/dtos/exception/ExceptionDTO.java
@@ -0,0 +1,5 @@
+package com.picpaysimplificado.dtos.exception;
+
+public record ExceptionDTO(String message, String statusCode) {
+    
+}
---
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.picpaysimplificado.dtos.exception.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicationEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+    
+}
